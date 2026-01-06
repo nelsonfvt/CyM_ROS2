18fa070b5a3118371647197e47ebc260c63ef8f9
Nelson <nelsonfvt@gmail.com>
2026-01-06 18:28:33 -0500

    robot CAD 3DOF added

---
+++ ROS2_sim/src/robot_3dof/robot_3dof/robot_3dof.py
@@ -0,0 +1,95 @@
+from math import sin, cos, pi
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import JointState
+from tf2_ros import TransformBroadcaster, TransformStamped
+
+class RobotControl(Node):
+
+    def __init__(self):
+        super().__init__('Robot_Control')
+        qos_profile = QoSProfile(depth=10)
+        self.joint_pub = self.create_publisher(JointState, 'joint_states',qos_profile)
+        self.broadcaster = TransformBroadcaster(self, qos=qos_profile)
+        self.nodeName = self.get_name()
+
+        self.get_logger().info("{0} started".format(self.nodeName))
+
+        #Estado inicial del robot
+        self.J1 = 0.0
+        self.J2 = 0.0
+        self.J3 = 0.0
+
+        self.J1_sent = 1.0
+        self.J2_sent = 1.0
+        self.J3_sent = 1.0
+
+        self.base_trans = TransformStamped()
+        self.base_trans.header.frame_id = 'map'
+        self.base_trans.child_frame_id = 'l_Base_link001'
+        self.joint_state = JointState()
+
+        self.Ts = 0.1
+        self.timer = self.create_timer(self.Ts, self.timer_callback)
+
+    def timer_callback(self):
+
+        self.get_logger().info(f"Publishing J_1: {self.J1} J_2: {self.J2} J_3: {self.J3}")
+        now = self.get_clock().now()
+
+        self.joint_state.header.stamp = now.to_msg()
+        self.joint_state.name = ['l_Base_link001__to__l_Link_004', 'l_Link_004__to__l_Link_005', 'l_Link_005__to__l_Link_006']
+        self.joint_state.position = [self.J1, self.J2, self.J3]
+
+        self.base_trans.header.stamp = now.to_msg()
+        self.base_trans.transform.translation.x = 0.10
+        self.base_trans.transform.translation.y = 0.10
+        self.base_trans.transform.translation.x = 0.10
+
+        # Quaternion methods
+        quat_tf = [0.0, 0.0, 0.0, 1.0]
+        quat = Quaternion(x=quat_tf[0], y=quat_tf[1], z=quat_tf[2], w=quat_tf[3])
+		
+        self.base_trans.transform.rotation = quat#Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
+		
+        self.joint_pub.publish(self.joint_state)
+        self.broadcaster.sendTransform(self.base_trans)
+
+        # Moviendo articulaciones del robot 3DOF
+        self.J1 += self.J1_sent * 0.1        
+        if self.J1 > pi/2:
+            self.J1_sent = -1.0
+        if self.J1 < -pi/2:
+            self.J1_sent = 1.0
+
+        self.J2 += self.J2_sent * 0.08        
+        if self.J2 > 0.0: #pi/2:
+            self.J2_sent = -1.0
+        if self.J2 < -pi/2: #0.0:
+            self.J2_sent = 1.0
+
+        self.J3 += self.J3_sent * 0.07        
+        if self.J3 > pi/2:
+            self.J3_sent = -1.0
+        if self.J3 < 0.0:
+            self.J3_sent = 1.0
+
+
+def main():
+
+    try:
+        rclpy.init()
+        robot_control = RobotControl()
+        rclpy.spin(robot_control)
+
+    except KeyboardInterrupt:
+        print(' ...Exit Node')
+
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ROS2_sim/src/robot_3dof/setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'robot_3dof'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share/' + package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+        (os.path.join('share/' + package_name, 'urdf'), glob(os.path.join('urdf', '*.*'))),
+        (os.path.join('share/' + package_name, 'rviz'), glob(os.path.join('rviz', '*.*'))),
+        (os.path.join('share/' + package_name, 'meshes'), glob(os.path.join('meshes', '*.*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nelsonvt',
+    maintainer_email='nelsonfvt@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'robot_3dof = robot_3dof.robot_3dof:main'
+        ],
+    },
+)
