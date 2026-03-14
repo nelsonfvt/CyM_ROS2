4de62151be18e39552eab55c3456dab656b89122
Nelson Velasco <nelsonfvt@gmail.com>
2026-03-14 16:26:45 -0500

    docker config added

---
+++ ROS2_basics/docker/workspace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Set ROS 2 distribution as a variable
+ROS_DISTRO="jazzy"
+
+# Source ROS 2 setup
+source /opt/ros/$ROS_DISTRO/setup.bash
+
+# Navigate to sim tutorial
+cd /root/ros2_ws
+
+# Install ROS2 dependencies for all packages
+#echo "Installing ROS 2 dependencies ROS2_basics..."
+#rosdep update
+#rosdep install -i --from-path src --rosdistro $ROS_DISTRO -y
+
+# Building the packages
+#echo "Building packages..."
+#colcon build
+#source install/setup.bash
+
+echo "Workspace setup completed!"
